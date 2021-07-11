d18deb001ac6c16ac78c8f4700f2b9e57f308a5f
tai2 <hello@tai2.net>
2021-07-11 16:52:38 +0900

    Add mute button

--- src/background.js
+++ src/background.js
@@ -1,5 +1,24 @@
+function getStorageValue(key, defaultValue) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get({ [key]: defaultValue }, (items) => {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError)
+      }
+      resolve(items[key])
+    })
+  })
+}
+
+function getMuted() {
+  return getStorageValue('mute', 'off').then((mute) => mute === 'on')
+}
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.disable()
+
+  getMuted().then((isMuted) => {
+    chrome.action.setBadgeText({ text: isMuted ? 'MUTE' : '' })
+  })
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,77 @@
-console.log('popup.js')
+init()
+
+async function init() {
+  const isMuted = await getMuted()
+  const muteButton = document.getElementById('muteButton')
+  muteButton.innerText = isMuted ? 'ミュート解除' : 'ミュート'
+  muteButton.addEventListener('click', async (ev) => {
+    const isMuted = await getMuted()
+    const newValue = !isMuted
+    muteButton.innerText = newValue ? 'ミュート解除' : 'ミュート'
+    chrome.action.setBadgeText({ text: newValue ? 'MUTE' : '' })
+    await setMuted(newValue)
+  })
+
+  const volumeInput = document.getElementById('volumeInput')
+  volumeInput.addEventListener('change', (ev) => {})
+
+  const pitchInput = document.getElementById('pitchInput')
+  pitchInput.addEventListener('change', (ev) => {})
+
+  const rateInput = document.getElementById('rateInput')
+  rateInput.addEventListener('change', (ev) => {})
+}
+
+function setMuted(isMuted) {
+  return setStorageValue('mute', isMuted ? 'on' : 'off')
+}
+
+function getMuted() {
+  return getStorageValue('mute', 'off').then((mute) => mute === 'on')
+}
+
+function setVolume(volume) {
+  return setStorageValue('volume', volume)
+}
+
+function getVolume() {
+  return getStorageValue('volume', 1.0)
+}
+
+function setPitch(pitch) {
+  return setStorageValue('pitch', pitch)
+}
+
+function getPitch() {
+  return getStorageValue('pitch', 1.0)
+}
+
+function setRate(rate) {
+  return setStorageValue('rate', rate)
+}
+
+function getRate() {
+  return getStorageValue('rate', 1.0)
+}
+
+function setStorageValue(key, value) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.set({ [key]: value }, () => {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError)
+      }
+      resolve()
+    })
+  })
+}
+
+function getStorageValue(key, defaultValue) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get({ [key]: defaultValue }, (items) => {
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError)
+      }
+      resolve(items[key])
+    })
+  })
+}
