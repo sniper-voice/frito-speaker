89ce974d7cc131c9de05556cce28090b9692c35f
tai2 <hello@tai2.net>
2021-07-23 11:26:41 +0900

    Transform successive eights to ぱちぱち

--- src/transformMessage.js
+++ src/transformMessage.js
@@ -1,8 +1,12 @@
 export function transformMessage(message) {
-  return message.replaceAll(
-    /([^a-zA-Zａ-ｚＡ-Ｚ]|^)([wｗ]+)([^a-zA-Zａ-ｚＡ-Ｚ]|$)/g,
-    (match, p1, p2, p3) => {
-      return p1 + 'くさ'.repeat(Math.min(p2.length, 3)) + p3
-    }
-  )
+  return message
+    .replaceAll(
+      /([^a-zA-Zａ-ｚＡ-Ｚ]|^)([wｗ]+)([^a-zA-Zａ-ｚＡ-Ｚ]|$)/g,
+      (match, p1, p2, p3) => {
+        return p1 + 'くさ'.repeat(Math.min(p2.length, 3)) + p3
+      }
+    )
+    .replaceAll(/([^\d]|^)(88+)([^\d]|$)/g, (match, p1, p2, p3) => {
+      return p1 + 'ぱち'.repeat(Math.min(p2.length, 4)) + p3
+    })
 }
--- src/transformMessage.test.js
+++ src/transformMessage.test.js
@@ -22,3 +22,25 @@ test('it transforms w mixed with hiragana', () => {
 test('it keeps w in English words', () => {
   expect(transformMessage('This is an window.')).toBe('This is an window.')
 })
+
+test('it keeps single 8', () => {
+  expect(transformMessage('8')).toBe('8')
+})
+
+test('it transforms 2 or more successive 8', () => {
+  expect(transformMessage('88')).toBe('ぱちぱち')
+  expect(transformMessage('888')).toBe('ぱちぱちぱち')
+  expect(transformMessage('8888')).toBe('ぱちぱちぱちぱち')
+  expect(transformMessage('88888')).toBe('ぱちぱちぱちぱち')
+})
+
+test('it transforms 8 mixed with hiragana', () => {
+  expect(transformMessage('おお888')).toBe('おおぱちぱちぱち')
+  expect(transformMessage('88すごい')).toBe('ぱちぱちすごい')
+})
+
+test('it keeps 8 in a number', () => {
+  expect(transformMessage('7889')).toBe('7889')
+  expect(transformMessage('889')).toBe('889')
+  expect(transformMessage('788')).toBe('788')
+})
